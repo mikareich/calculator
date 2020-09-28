34f58ccf3f20eae6bc040d881d0910d20c9c7230
mikareich <mikareich0@gmail.com>
2020-09-28 19:28:25 +0200

    install dependencies

---
+++ public/bundle.js
@@ -0,0 +1,1 @@
+!function(e,t){e.getElementById("livereloadscript")||((t=e.createElement("script")).async=1,t.src="//"+(window.location.host||"localhost").split(":")[0]+":35729/livereload.js?snipver=1",t.id="livereloadscript",e.getElementsByTagName("head")[0].appendChild(t))}(window.document);
