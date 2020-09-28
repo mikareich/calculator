13168252ddc2c3ecec0dfb38e2709ff5d7da633f
mikareich <mikareich0@gmail.com>
2020-09-28 20:52:35 +0200

    add js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
+import Mexp from 'math-expression-evaluator'
 import './styles.scss'
+
+const buttons = document.querySelectorAll('button')
+const outputSPAN = document.querySelector('.output')
+let input = ''
+
+const taskRefferences = {
+  addCharacter,
+  clear,
+  calculate
+}
+
+// add task to button
+buttons.forEach((button) => {
+  const task = button.dataset.task
+  const { textContent } = button
+  const taskRefferenc = taskRefferences[task]
+
+  button.onclick = () => {
+    // execute specific task
+    taskRefferenc(textContent)
+    // render result
+    outputSPAN.innerHTML = input
+    // clear if nescessary
+    if (task === 'calculate') clear()
+  }
+})
+
+function addCharacter(character) {
+  input += character
+}
+function clear() {
+  input = ''
+}
+function calculate() {
+  // parse input and replace special characters
+  const parsedInput = input
+    .replaceAll('%', '/100')
+    .replaceAll(',', '.')
+    .replaceAll('÷', '/')
+    .replaceAll('×', '*')
+
+  try {
+    input = Mexp.eval(parsedInput)
+  } catch (error) {
+    input = error.message
+  }
+}
